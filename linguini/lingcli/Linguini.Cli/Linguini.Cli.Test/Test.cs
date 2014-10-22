83390e5c8954da2cb64c062c4e3abfc01d445b4e
Christopher MANEU <christopher@maneu.net>
2014-10-22 11:30:05 +0200

    Add project content

---
+++ linguini/lingcli/Linguini.Cli/Linguini.Cli.Test/Test.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Linguini.Cli.Test
+{
+	[TestFixture ()]
+	public class Test
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+			Assert.IsTrue (false, "Every CLI is okay");
+		}
+	}
+}
+
